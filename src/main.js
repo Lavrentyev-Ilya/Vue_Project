c6b559515dcdd642614f93a04c94d91d39f8efb2
Ilya Lavrentyev <ilyalavrentyev@gmail.com>
2021-12-12 23:11:27 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
-=======
 
 createApp(App).mount('#app')
->>>>>>> 661c6fe (init)
